d680d652d0e99e21957c5c78cc3636749185c6fc
wugi <17031278@itcelaya.edu.mx>
2023-04-09 20:59:44 -0600

    Working button

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,13 +1,29 @@
+import React, {useState} from "react";
+import memesData from "../memesData.js";
+
 export default function Meme()
 {
+   const [memeUrl, changeMemeUrl] = useState(0);
+
+   function pure()
+   {   
+      event.preventDefault()
+      const memesArray = memesData.data.memes;
+      const image = memesArray[Math.floor(Math.random() * memesArray.length)].url;
+
+      changeMemeUrl(image);
+   }
+
    return(
       <main>
          <form className="form">
             <input className="form-input" placeholder="Top text" type="text"/>
             <input className="form-input" placeholder="Bottom text" type="text"/>
 
-            <button className="form-button">Get a new meme image 🖼️</button>
+            <button onClick={pure} className="form-button">Get a new meme image 🖼️</button>
          </form>
+
+         <img className="image-result" src={memeUrl} ></img>
       </main>
    )
-}t
+}
